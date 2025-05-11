c2cf1427e29ffe95476ce5ad85dfa8b60f93857b
nexpid <me@nexpid.xyz>
2025-05-11 15:10:41 +0200

    fix(stuff): don't use RN prefix for js deps

--- src/plugins/cloud-sync/src/stuff/files.ts
+++ src/plugins/cloud-sync/src/stuff/files.ts
@@ -1,65 +1,70 @@
-import { RNDocumentPicker, RNDocuments, RNFileModule } from "$/deps";
+import { DocumentPicker, DocumentsNew, RNFileModule } from "$/deps";
 
 function parseLink(link: string) {
-    const path = link.split("://");
-    return `/${path[1]}`;
+	const path = link.split("://");
+	return `/${path[1]}`;
 }
 
 export async function pickFile(): Promise<string | null> {
-    let text: string | null = null;
+	let text: string | null = null;
 
-    if (RNDocumentPicker) {
-        try {
-            const { fileCopyUri, type } =
-                await RNDocumentPicker.pickSingle({
-                    type: RNDocumentPicker.types.plainText,
-                    mode: 'open',
-                    copyTo: 'cachesDirectory',
-                });
-            if (type === "text/plain" && fileCopyUri) text = await RNFileModule.readFile(parseLink(fileCopyUri), "utf8");
-        } catch (err: any) {
-            if (!RNDocumentPicker.isCancel(err)) throw new Error(err);
-        }
-    } else if (RNDocuments) {
-        const [{ uri, name, error, type }] = await RNDocuments.pick({
-            type: RNDocuments.types.plainText,
-            allowVirtualFiles: true,
-            mode: "import",
-        });
-        if (error) throw new Error(error);
+	if (DocumentPicker) {
+		try {
+			const { fileCopyUri, type } = await DocumentPicker.pickSingle({
+				type: DocumentPicker.types.plainText,
+				mode: "open",
+				copyTo: "cachesDirectory",
+			});
+			if (type === "text/plain" && fileCopyUri) {
+				text = await RNFileModule.readFile(parseLink(fileCopyUri), "utf8");
+			}
+		} catch (err: any) {
+			if (!DocumentPicker.isCancel(err)) throw new Error(err);
+		}
+	} else if (DocumentsNew) {
+		const [{ uri, name, error, type }] = await DocumentsNew.pick({
+			type: DocumentsNew.types.plainText,
+			allowVirtualFiles: true,
+			mode: "import",
+		});
+		if (error) throw new Error(error);
+		else if (type === "text/plain" && uri) {
+			const [copyResult] = await DocumentsNew.keepLocalCopy({
+				files: [{
+					fileName: name ?? "cloudsync.txt",
+					uri,
+				}],
+				destination: "cachesDirectory",
+			});
+			console.log(uri);
+			if (copyResult.status === "success") {
+				console.log(copyResult.localUri);
+				text = await RNFileModule.readFile(parseLink(copyResult.localUri), "utf8");
+			} else {
+				throw new Error(copyResult.copyError);
+			}
+		}
+	}
 
-        else if (type === "text/plain" && uri) {
-            const [copyResult] = await RNDocuments.keepLocalCopy({
-                files: [{
-                    fileName: name ?? "cloudsync.txt",
-                    uri
-                }],
-                destination: "cachesDirectory"
-            });
-            console.log(uri);
-            if (copyResult.status === "success") {
-                console.log(copyResult.localUri);
-                text = await RNFileModule.readFile(parseLink(copyResult.localUri), "utf8");
-            } else {
-                throw new Error(copyResult.copyError);
-            }
-        }
-    }
-
-    return text;
+	return text;
 }
 
 export function canSaveFileNatively() {
-    return !!RNDocuments;
+	return !!DocumentsNew;
 }
 
 export async function saveFile(name: string, content: any) {
-    if (!RNDocuments) return false;
+	if (!DocumentsNew) return false;
 
-    const path = await RNFileModule.writeFile("cache", `cloudsync_${name}`, content, "utf8");
-    return await RNDocuments.saveDocuments({
-        sourceUris: [`file:///${path}`],
-        mimeType: "text/plain",
-        fileName: name,
-    }).then(x => x[0]);
+	const path = await RNFileModule.writeFile(
+		"cache",
+		`cloudsync_${name}`,
+		content,
+		"utf8",
+	);
+	return await DocumentsNew.saveDocuments({
+		sourceUris: [`file:///${path}`],
+		mimeType: "text/plain",
+		fileName: name,
+	}).then(x => x[0]);
 }
